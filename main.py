d737b74138bf3fd325cefe78435a8b845fc2ed89
radl97 <radl97@users.noreply.github.com>
2020-05-26 15:25:06 +0200

    Use JSON instead of pythonic output

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from youtube_dl.options import parseOpts
+import json
 
 (parser, _, _) = parseOpts()
 
-print(parser.data)
+for optionGroup in parser.data:
+	for option in optionGroup['options']:
+		if 'type' in option['named']:
+			v = option['named']['type']
+			# "type" cannot be encoded. Use the name of it instead.
+			if isinstance(v, type):
+				v = v.__name__
+			option['named']['type'] = v
+		# function cannot be encoded, we can just note that there is one
+		if 'callback' in option['named']:
+			option['named']['callback'] = True
+print(json.dumps({'data': parser.data}))
